33b5142601f383358fd8e64e2b131328f668cd41
Teejay012 <tijesunimioluwakoya@gmail.com>
2024-10-07 11:55:43 +0100

    first commit

--- src/voom_frontend/src/componentss/menu/Menu.jsx
+++ src/voom_frontend/src/componentss/menu/Menu.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Menu = () => {
   return (
     <div className='bg-[#F8F9FB] p-4 flex flex-col gap-2 absolute top-[100%] left-0 z-20 w-[100px] rounded-[10px] shadow-md'>
-        <a href="./register" className="text-[16px] text-[#1D1E20] font-[600]">Sign Up</a>
-        <a href="./sign-in" className="text-[16px] text-[#1D1E20] font-[600]">Sign In</a>
+        <Link to="./register" className="text-[16px] text-[#1D1E20] font-[600]">Sign Up</Link>
+        <Link to="./sign-in" className="text-[16px] text-[#1D1E20] font-[600]">Sign In</Link>
     </div>
   )
 }
