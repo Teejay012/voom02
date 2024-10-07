434522501d313db9bf69af2851169f6636c023d1
Teejay012 <tijesunimioluwakoya@gmail.com>
2024-10-07 11:51:31 +0100

    first commit

--- src/voom_frontend/src/componentss/moreNav/MoreNav.jsx
+++ src/voom_frontend/src/componentss/moreNav/MoreNav.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 import { IoNotifications } from "react-icons/io5";
 import { BiSolidStore } from "react-icons/bi";
 import { IoChatbubbleEllipsesSharp } from "react-icons/io5";
@@ -8,24 +9,24 @@ const MoreNav = () => {
     <div className="fixed right-0 bottom-[70px] z-50 h-[70%] p-3 w-[70px] bg-[#fff] rounded-[15px] shadow-md">
         <ul className="flex flex-col gap-5 items-center justify-end w-full h-full">
             <li>
-                <a href="./notification" className="flex flex-col items-center justify-center gap-2">
+                <Link to="./notification" className="flex flex-col items-center justify-center gap-2">
                     <IoNotifications className="text-[25px] font-bold"/>
                     <p className="text-[12px] text-[#8891A5] font[500]">Notification</p>
-                </a>
+                </Link>
             </li>
 
             <li>
-                <a href="#" className="flex flex-col items-center justify-center gap-2">
+                <Link href="#" className="flex flex-col items-center justify-center gap-2">
                     <BiSolidStore className="text-[25px] font-bold"/>
                     <p className="text-[12px] text-[#8891A5] font[500]">Stores</p>
-                </a>
+                </Link>
             </li>
 
             <li>
-                <a href="#" className="flex flex-col items-center justify-center gap-2">
+                <Link href="#" className="flex flex-col items-center justify-center gap-2">
                     <IoChatbubbleEllipsesSharp className="text-[25px] font-bold"/>
                     <p className="text-[12px] text-[#8891A5] font[500]">Inbox</p>
-                </a>
+                </Link>
             </li>
         </ul>
     </div>
